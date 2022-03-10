35d7ab86995343e45bedd02d4cd92fe5cc2455ad
rijabaig <90227392+rijabaig@users.noreply.github.com>
2022-03-10 12:44:13 -0500

    Update TableMaker.java

--- lab05/TableMaker.java
+++ lab05/TableMaker.java
@@ -24,22 +24,26 @@ public class TableMaker extends Application {
         stage.setHeight(500);
         final Label lb = new Label("Marks Table");
         lb.setFont(new Font("Arial", 20));
+        TableColumn<StudentRecord, Float> assignmentCol = new TableColumn<>("Assignments");
+        assignmentCol.setMinWidth(200);
+
+        assignmentCol.setCellValueFactory(new PropertyValueFactory<StudentRecord, Float>("assignment"));
 
         TableColumn<StudentRecord, String> SIDCol = new TableColumn<>("SID");
         SIDCol.setMinWidth(200);
 
         SIDCol.setCellValueFactory(new PropertyValueFactory<StudentRecord, String>("id"));
 
-        TableColumn<StudentRecord, Float> assignmentCol = new TableColumn<>("Assignments");
-        assignmentCol.setMinWidth(200);
-
-        assignmentCol.setCellValueFactory(new PropertyValueFactory<StudentRecord, Float>("assignment"));
-
         TableColumn<StudentRecord, Float> midtermCol = new TableColumn<>("Midterm");
         midtermCol.setMinWidth(200);
 
         midtermCol.setCellValueFactory(new PropertyValueFactory<StudentRecord, Float>("midterm"));
+        TableColumn<StudentRecord, Character> letterCol = new TableColumn<>("Letter Grade");
+        letterCol.setMinWidth(200);
 
+        letterCol.setCellValueFactory(new PropertyValueFactory<StudentRecord, Character>("letterGrade"));
+
+        TableView<StudentRecord> table = new TableView<>();
         TableColumn<StudentRecord, Float> finalCol = new TableColumn<>("Final Exam");
         finalCol.setMinWidth(200);
 
@@ -49,12 +53,6 @@ public class TableMaker extends Application {
         totalCol.setMinWidth(200);
         totalCol.setCellValueFactory(new PropertyValueFactory<StudentRecord, Float>("total"));
 
-        TableColumn<StudentRecord, Character> letterCol = new TableColumn<>("Letter Grade");
-        letterCol.setMinWidth(200);
-
-        letterCol.setCellValueFactory(new PropertyValueFactory<StudentRecord, Character>("letterGrade"));
-
-        TableView<StudentRecord> table = new TableView<>();
         table.setItems(DataSource.getAllMarks());
         table.getColumns().addAll(SIDCol, assignmentCol, midtermCol,
                 finalCol, totalCol, letterCol);
